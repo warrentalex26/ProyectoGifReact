d3400c052e9deb514369198c8924be3d84c87405
warrentalex26 <warrentalex26ies@gmail.com>
2021-02-23 14:10:59 -0600

    creando lista, recorrer lista, mutar la lista agregando elemento al array

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 
 const GifExpertApp = () => {
+
+    //const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball'])
+    
+    const handledAdd = () => {
+      /*AGREGAR ELEMENTOS AL ARREGLO DE ESTA MANERA PARA CAMBIAR EL ESTADO*/ 
+      // setCategories([...categories, 'push']);
+      // setCategories(['push', ...categories]);
+      setCategories(cate => [...cate, 'Hola']);
+
+    }
+    
+    // el key no puede ser el indice y debe ser unico
     return (
         <>
-          <h2>GifExpertApp</h2>  
+          <h2>GifExpertApp</h2>
           <hr/>
+          <button onClick={handledAdd}>Agregar</button>
+          
+          <ol>
+              {categories.map(categoria => {
+                return <li key={categoria}>{categoria}</li>
+              })}
+          </ol>
         </>
     )
 }
