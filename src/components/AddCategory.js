82795f1dae473d6ec276d9a1cbed1b9f8ad6167a
warrentalex26 <warrentalex26ies@gmail.com>
2021-02-23 15:20:54 -0600

    leer evento del teclado, enviar formulario con onSubmit

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -3,20 +3,24 @@ import React, { Component, useState} from 'react'
 export const AddCategory =  () =>  {
 
     const [inputValue, setInputValue] = useState('');
+    
     const handleInputChange = (event) => {
         console.log(event.target.value)
-        setInputValue(event.target.value)
+        setInputValue(event.target.value) //Actualizar el estado del input
+    }
 
+    const handleSubmit = (event) => {
+        event.preventDefault(); //Prevenir que recargue el navegador
+        
     }
     
         return (
-            <>
-            <h1>{inputValue}</h1>
+            <form onSubmit={handleSubmit}> 
                 <input type="text"
                 placeholder="buscar"
                 value={inputValue}
                 onChange={handleInputChange}/>
-            </>
+            </form>
         )
     }
 
