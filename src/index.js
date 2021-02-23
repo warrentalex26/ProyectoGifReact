edc7c699944378dc4562a5b4da3956c85e043846
warrentalex26 <warrentalex26ies@gmail.com>
2021-02-23 13:30:36 -0600

    Inicio del proyecto

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GifExpertApp from './GifExpertApp';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GifExpertApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
