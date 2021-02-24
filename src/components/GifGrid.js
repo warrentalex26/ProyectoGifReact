d235b292225ab8082f78c9463e4cb2fadef84d82
warrentalex26 <warrentalex26ies@gmail.com>
2021-02-24 08:43:08 -0600

    useEffect

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const GifGrid = ({category}) => {
 
+    const [count, setCount] = useState(0);
+
+    useEffect( () => { //Solo se ejecute esta instrucción cuando el componente es renderizado por primera vez
+        getGifs()
+    }, [])
+
     const getGifs = async() => { //Usamos Async y await para hacer una peticion asincrona
-        const url = `https://api.giphy.com/v1/gifs/search?q=Hola&limit=10&api_key=BHzrVw5bm8RmxyqDHF6i4qP9iH3UesCm`; //Esa es la url para hacer las peticiones GET a giphy
+        const api = `BHzrVw5bm8RmxyqDHF6i4qP9iH3UesCm`
+        const url = `https://api.giphy.com/v1/gifs/search?q=Hola&limit=10&api_key=${api}`; //Esa es la url para hacer las peticiones GET a giphy
         const resp = await fetch(url); //Usamos el fetch para consumir los datos
         const {data} = await resp.json(); // Los datos los pasamos a un formato JSON
         const gifs = data.map( gifImg => { // Mandé la data al console.log para ver los datos y de ahi fui sacando las propiedades que manda la api, además recorremos el arreglo con map
@@ -18,12 +25,12 @@ const GifGrid = ({category}) => {
 
     }
 
-    getGifs()
-
 
     return (
         <div>
             <h3>{category}</h3>
+            <h2>{count}</h2>
+            <button onClick={ () => setCount(count + 1)}>Boton</button>
         </div>
     )
 }
